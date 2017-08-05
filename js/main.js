af918763cf0a6468578e10c2f5bbdd874b99588d
FieryArrows <edward.andrade27@gmail.com>
2017-08-05 04:31:40 -0700

    My first website

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// music background
+
+var audio, playbtn, seekbar_bar;
+function initAudioPlayer() {
+	audio = new Audio();
+	audio.src = "music/birthday.mp3";
+	audio.loop = false;
+	audio.play();
+}
+
+window.addEventListener("load", initAudioPlayer);
